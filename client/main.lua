10ab1110d5c67deb229923e222dc19bd83932d90
cynio <115213364+dominiksmolinski3@users.noreply.github.com>
2023-07-28 13:27:11 +0200

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -29,24 +29,21 @@ if Config.ObjectEnabled then
 	    while true do
 	        Wait(1)
 	        for k,v in pairs(Config.Objects) do
-		        	local player = GetPlayerPed(-1)
+		        	local player = PlayerPedId()
 		        	local distanceobject = 2.2
 		            local obj = GetClosestObjectOfType(GetEntityCoords(player).x, GetEntityCoords(player).y, GetEntityCoords(player).z, distanceobject, v, false, true ,true)
 		            local distance = GetDistanceBetweenCoords(GetEntityCoords(player), GetEntityCoords(obj), true)
-		    
-
-
 			    if distance <= distanceobject then
 
 		            local ObjectCoords = GetEntityCoords(obj)
 
 		            if not objects[obj] then
-		            ESX.Game.Utils.DrawText3D(ObjectCoords + vector3(0.0, 0.0, 0.5), '~c~Search~b~[E]~c~ ', 1, 4) -- here you can customise what appears on a prop that is searchable
+						ESX.Game.Utils.DrawText3D(ObjectCoords + vector3(0.0, 0.0, 0.5), '~c~Search~b~[E]~c~ ', 1, 4) -- here you can customise what appears on a prop that is searchable
 		        	end
 
 		            if IsControlJustReleased(0, 38) then -- here you can change hotkey that is used to loot, default 'E' (38)
 						if not objects[obj] then
-		                    SetCurrentPedWeapon(GetPlayerPed(-1), 0xA2719263, true)
+		                    SetCurrentPedWeapon(PlayerPedId(), 0xA2719263, true)
 		                    RequestAnimDict("anim@amb@clubhouse@tutorial@bkr_tut_ig3@")
 							Citizen.Wait(1000)
 							TaskPlayAnim(PlayerPedId(), "anim@amb@clubhouse@tutorial@bkr_tut_ig3@", "machinic_loop_mechandplayer", 2.0, 2.0, 5000, 30, 0, 0, 0, 0)
