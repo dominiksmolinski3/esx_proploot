a368b8a30748e6c8e779f3e227e7b85dbba6abce
cynio <115213364+dominiksmolinski3@users.noreply.github.com>
2023-07-28 04:52:33 +0200

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -4,8 +4,25 @@ RegisterNetEvent('esx:playerLoaded')
 AddEventHandler('esx:playerLoaded', function(xPlayer)
 	PlayerData = xPlayer
 end)
-
 objects = {}
+function loot()
+randomChance = math.random(1, 100)
+randomWeapon = Config.WeaponLoot[math.random(1, #Config.WeaponLoot)]
+randomItem = Config.ItemLoot[math.random(1, #Config.ItemLoot)]
+
+if randomChance > 0 and randomChance < Config.ProbabilityWeaponLoot then
+	local randomAmmo = math.random(12, 48) -- here you can choose between which numbers amount ammo will be given to weapon if you loot it
+	GiveWeaponToPed(GetPlayerPed(-1), randomWeapon, randomAmmo, true, false)
+	exports['mythic_notify']:DoHudText('inform','You have found a weapon.')
+elseif randomChance >= Config.ProbabilityWeaponLoot and randomChance < Config.ProbabilityMoneyLoot then
+	TriggerServerEvent('cynio:money')
+elseif randomChance >= Config.ProbabilityMoneyLoot and randomChance < Config.ProbabilityItemLoot then
+	TriggerServerEvent('cynio:item', randomItem)
+elseif randomChance >= Config.ProbabilityItemLoot and randomChance < 100 then
+	exports['mythic_notify']:DoHudText('inform','You did not find anything...') -- here you can edit message for your language
+	end
+end
+
 
 if Config.ObjectEnabled then
 	Citizen.CreateThread(function()
@@ -38,22 +55,7 @@ if Config.ObjectEnabled then
 							end
 							TaskPlayAnim(PlayerPedId(), "anim@amb@clubhouse@tutorial@bkr_tut_ig3@", "machinic_loop_mechandplayer", 2.0, 2.0, 5000, 30, 0, 0, 0, 0)
 							Citizen.Wait(5000)
-	
-							randomChance = math.random(1, 100)
-							randomWeapon = Config.WeaponLoot[math.random(1, #Config.WeaponLoot)]
-							randomItem = Config.ItemLoot[math.random(1, #Config.ItemLoot)]
-
-							if randomChance > 0 and randomChance < Config.ProbabilityWeaponLoot then
-								local randomAmmo = math.random(12, 48) -- here you can choose between which numbers amount ammo will be given to weapon if you loot it
-								GiveWeaponToPed(GetPlayerPed(-1), randomWeapon, randomAmmo, true, false)
-								exports['mythic_notify']:DoHudText('inform','You have found a weapon.')
-							elseif randomChance >= Config.ProbabilityWeaponLoot and randomChance < Config.ProbabilityMoneyLoot then
-								TriggerServerEvent('cynio:money')
-							elseif randomChance >= Config.ProbabilityMoneyLoot and randomChance < Config.ProbabilityItemLoot then
-								TriggerServerEvent('cynio:item', randomItem)
-							elseif randomChance >= Config.ProbabilityItemLoot and randomChance < 100 then
-								exports['mythic_notify']:DoHudText('inform','You did not find anything...') -- here you can edit message for your language
-							end
+							loot()
 							objects[obj] = true
 		               	end
 		            end
