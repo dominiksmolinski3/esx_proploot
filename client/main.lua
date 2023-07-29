1556b65877202eaffd8e1a77501ca265db003c26
cynio <115213364+dominiksmolinski3@users.noreply.github.com>
2023-07-29 02:16:26 +0200

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -6,7 +6,7 @@ AddEventHandler('esx:playerLoaded', function(xPlayer)
     PlayerData = xPlayer
 end)
  
-function loot(xPlayer)
+local function loot(xPlayer)
     local randomChance = math.random(1, 100)
     local randomWeapon = Config.WeaponLoot[math.random(1, #Config.WeaponLoot)]
     local randomItem = Config.ItemLoot[math.random(1, #Config.ItemLoot)]
@@ -29,27 +29,27 @@ if Config.ObjectEnabled then
 	    while true do
 	        Citizen.Wait(5)
 	        for k,v in pairs(Config.Objects) do
-		        	local player = PlayerPedId()
-		        	local distanceobject = 2.2
+		            local player = PlayerPedId()
+		            local distanceobject = 2.2
 		            local obj = GetClosestObjectOfType(GetEntityCoords(player).x, GetEntityCoords(player).y, GetEntityCoords(player).z, distanceobject, v, false, true ,true)
-		            local distance = GetDistanceBetweenCoords(GetEntityCoords(player), GetEntityCoords(obj), true)
-			    if distance <= distanceobject then
+		            local distance = #(GetEntityCoords(player) - GetEntityCoords(obj))
+			 if distance <= distanceobject then
 
 		            local ObjectCoords = GetEntityCoords(obj)
 
 		            if not objects[obj] then
-						ESX.Game.Utils.DrawText3D(ObjectCoords + vector3(0.0, 0.0, 0.5), 'Przeszukaj', 1, 4) -- here you can customise what appears on a prop that is searchable
-		        	end
+				ESX.Game.Utils.DrawText3D(ObjectCoords + vector3(0.0, 0.0, 0.5), 'Search', 1, 4) -- here you can customise what appears on a prop that is searchable
+		            end
 
 		            if IsControlJustReleased(0, 38) then -- here you can change hotkey that is used to loot, default 'E' (38)
-						if not objects[obj] then
+				if not objects[obj] then
 		                    SetCurrentPedWeapon(PlayerPedId(), 0xA2719263, true)
 		                    RequestAnimDict("anim@amb@clubhouse@tutorial@bkr_tut_ig3@")
-							Citizen.Wait(1000)
-							TaskPlayAnim(PlayerPedId(), "anim@amb@clubhouse@tutorial@bkr_tut_ig3@", "machinic_loop_mechandplayer", 2.0, 2.0, 5000, 30, 0, 0, 0, 0)
-							Citizen.Wait(5000)
-							loot()
-							objects[obj] = true
+				    Citizen.Wait(1000)
+				    TaskPlayAnim(PlayerPedId(), "anim@amb@clubhouse@tutorial@bkr_tut_ig3@", "machinic_loop_mechandplayer", 2.0, 2.0, 5000, 30, 0, 0, 0, 0)
+				    Citizen.Wait(5000)
+				    loot()
+				    objects[obj] = true
 		               	end
 		            end
 		        end
