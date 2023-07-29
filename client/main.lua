279f90302644a3e42db1a8803510c866f95d9e9d
cynio <115213364+dominiksmolinski3@users.noreply.github.com>
2023-07-29 15:21:09 +0200

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -24,6 +24,12 @@ local function loot(xPlayer)
     end
 end
  
+ local function napis(ObjectCoords)
+	if not objects[obj] then
+		ESX.Game.Utils.DrawText3D(ObjectCoords + vector3(0.0, 0.0, 0.5), 'Search', 1, 4) -- here you can customise what appears on a prop that is searchable
+	end
+end
+
 if Config.ObjectEnabled then
 	Citizen.CreateThread(function()
 	    while true do
@@ -34,26 +40,23 @@ if Config.ObjectEnabled then
 		            local obj = GetClosestObjectOfType(GetEntityCoords(player).x, GetEntityCoords(player).y, GetEntityCoords(player).z, distanceobject, v, false, true ,true)
 		            local distance = #(GetEntityCoords(player) - GetEntityCoords(obj))
 			 if distance <= distanceobject then
-
-		            local ObjectCoords = GetEntityCoords(obj)
-
-		            if not objects[obj] then
-				ESX.Game.Utils.DrawText3D(ObjectCoords + vector3(0.0, 0.0, 0.5), 'Search', 1, 4) -- here you can customise what appears on a prop that is searchable
-		            end
+					local ObjectCoords = GetEntityCoords(obj)
+		            napis(ObjectCoords)
 
 		            if IsControlJustReleased(0, 38) then -- here you can change hotkey that is used to loot, default 'E' (38)
-				if not objects[obj] then
-		                    SetCurrentPedWeapon(PlayerPedId(), 0xA2719263, true)
-		                    RequestAnimDict("anim@amb@clubhouse@tutorial@bkr_tut_ig3@")
-				    Citizen.Wait(1000)
-				    TaskPlayAnim(PlayerPedId(), "anim@amb@clubhouse@tutorial@bkr_tut_ig3@", "machinic_loop_mechandplayer", 2.0, 2.0, 5000, 30, 0, 0, 0, 0)
-				    Citizen.Wait(5000)
-				    loot()
-				    objects[obj] = true
+						if not objects[obj] then
+							SetCurrentPedWeapon(PlayerPedId(), 0xA2719263, true)
+							RequestAnimDict("anim@amb@clubhouse@tutorial@bkr_tut_ig3@")
+							Citizen.Wait(1000)
+							TaskPlayAnim(PlayerPedId(), "anim@amb@clubhouse@tutorial@bkr_tut_ig3@", "machinic_loop_mechandplayer", 2.0, 2.0, 5000, 30, 0, 0, 0, 0)
+							Citizen.Wait(5000)
+							loot()
+							objects[obj] = true
 		               	end
 		            end
 		        end
 		    end
 		end
+		Citizen.Wait(1000)
 	end)
 end
